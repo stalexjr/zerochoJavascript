8c84898196e5f296bc9b6c0a7a91fcc5182ad8c7
woojin <76862976+stalexjr@users.noreply.github.com>
2022-09-23 07:16:10 +0900

    Add files via upload

--- 로또추첨기/js/script.js
+++ 로또추첨기/js/script.js
@@ -1,3 +1,61 @@
-const candidate = Array(45).fill().map((v, i) => {
-  return i + 1;
-});
+const candidate = Array(45).fill().map((v, i) => i + 1);
+
+const shuffle = [];
+while (candidate.length > 0){
+  const random = Math.floor(Math.random() * candidate.length); //무작위 인덱스 뽑기
+  const spliceArray = candidate.splice(random, 1); // 뽑은 값은 배열에 들어있음
+  const value = spliceArray[0]; //배열에 들어있는 값을 꺼내어
+  shuffle.push(value); // shuffle 배열에 넣기
+}
+console.log(shuffle);
+const winBalls = shuffle.slice(0, 6).sort((a, b) => a - b );
+const bonus = shuffle[6];
+console.log(winBalls, bonus);
+
+const showBall = (number, $target) => {
+  
+  const $ball = document.createElement('div');
+  $ball.className = 'ball';
+  $ball.textContent = number;
+  $target.appendChild($ball);
+};
+
+const $result = document.querySelector('#result');
+const $bonus = document.querySelector('#bonus');
+setTimeout(() =>{
+  showBall(winBalls[0], $result);
+}, 1000);
+setTimeout(() =>{
+  showBall(winBalls[1], $result);
+}, 2000);
+setTimeout(() =>{
+  showBall(winBalls[2], $result);
+}, 3000);
+setTimeout(() =>{
+  showBall(winBalls[3], $result);
+}, 4000);
+setTimeout(() =>{
+  showBall(winBalls[4], $result);
+}, 5000);
+setTimeout(() =>{
+  showBall(winBalls[5], $result);
+}, 6000);
+setTimeout(() =>{
+  showBall(bonus, $bonus);
+}, 7000);
+// for(let i = 0; i < candidate.length; i+1){
+//     const random = Math.floor(Math.random() * candidate.length); //무작위 인덱스 뽑기
+//     const spliceArray = candidate.splice(random, 1); // 뽑은 값은 배열에 들어있음
+//     const value = spliceArray[0]; //배열에 들어있는 값을 꺼내어
+//     shuffle.push(value); // shuffle 배열에 넣기
+//   }
+// for(let i = candidate.length; i > 0; i--){
+//   const random = Math.floor(Math.random() * candidate.length); //무작위 인덱스 뽑기
+//   const spliceArray = candidate.splice(random, 1); // 뽑은 값은 배열에 들어있음
+//   const value = spliceArray[0]; //배열에 들어있는 값을 꺼내어
+//   shuffle.push(value); // shuffle 배열에 넣기
+// }
+
+// 조건이 간단하거나, 몇번 반복해야할지 정확히 감이 안올때 while문, 
+// 조건이 복잡하거나 몇번 반복할지 정확히 알고 있을때 for문
+// splice는 기존 배열에 영향을 주고 처음에는 인덱스번호 두번째는 갯수고 slice은 기존 배열에 영향을 주지 않고 둘다 인덱스 번호를 뜻하고 처음 인덱스 번호는 포함 두번째 인덱스 번호는 미포함을 뜻한다.
