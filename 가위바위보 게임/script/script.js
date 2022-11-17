d3dae681808551fcb881e796ecdfbdf8af1c071c
woojin <76862976+stalexjr@users.noreply.github.com>
2022-11-18 05:34:42 +0900

    Add files via upload

---
+++ 가위바위보 게임/script/script.js
@@ -0,0 +1,10 @@
+const $computer = document.querySelector('#computer');
+const $score = document.querySelector('#score');
+const $rock = document.querySelector('#rock');
+const $scissors = document.querySelector('#scissors');
+const $paper = document.querySelector('#paper');
+const IMG_URL = './rsp.png';
+
+$computer.style.background = 'url(${IMG_URL}) 0 0';
+$computer.stlye.backgroundSize = 'auto 200px';
+
