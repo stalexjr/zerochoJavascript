04fa702b5618045223f557edcd4a9e818f0090b9
woojin <76862976+stalexjr@users.noreply.github.com>
2022-11-24 06:33:38 +0900

    Add files via upload

--- 가위바위보 게임/script/script.js
+++ 가위바위보 게임/script/script.js
@@ -31,5 +31,18 @@ const changeComputerHand = () =>{
 	$computer.style.background = `url(${IMG_URL}) ${rspX[computerChoise]} 0`;
 	$computer.style.backgroundSize = 'auto 200px';
 }
-setInterval(changeComputerHand, 50);
+let intervalId = setInterval(changeComputerHand, 50);
 
+// clickButton 5번 호출, 1번 인터벌 ~ 5번 인터벌로 계속 중첩됨 
+// 그 다움에 버튼을 클릭하면 1번부터 5번 인터벌중 5번 인터벌만 취소함 그래서 1번부터 4번까지의 인터벌은 계속 작동하게 되는것임
+const clickButton = () => {
+	clearInterval(intervalId);
+	setTimeout(()=>{
+		clearInterval(intervalId);
+		intervalId = setInterval(changeComputerHand, 50);
+	}, 1000);
+}
+
+$rock.addEventListener('click', clickButton);
+$scissors.addEventListener('click', clickButton);
+$paper.addEventListener('click', clickButton);
