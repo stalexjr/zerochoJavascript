0ef67c8dc1cf1f9002ee3e6ab70ad63953cdd244
woojin <76862976+stalexjr@users.noreply.github.com>
2022-11-25 06:18:38 +0900

    Add files via upload

--- 가위바위보 게임/script/script.js
+++ 가위바위보 게임/script/script.js
@@ -35,13 +35,34 @@ let intervalId = setInterval(changeComputerHand, 50);
 
 // clickButton 5번 호출, 1번 인터벌 ~ 5번 인터벌로 계속 중첩됨 
 // 그 다움에 버튼을 클릭하면 1번부터 5번 인터벌중 5번 인터벌만 취소함 그래서 1번부터 4번까지의 인터벌은 계속 작동하게 되는것임
-const clickButton = () => {
-	clearInterval(intervalId);
-	setTimeout(()=>{
+// const clickButton = () => {
+// 	clearInterval(intervalId);
+// 	$rock.removeEventListener('click', clickButton);
+// 	$scissors.removeEventListener('click', clickButton);
+// 	$paper.removeEventListener('click', clickButton);
+
+// 	setTimeout(()=>{
+// 		clearInterval(intervalId);
+// 		intervalId = setInterval(changeComputerHand, 50);
+// 		$rock.addEventListener('click', clickButton);
+// 		$scissors.addEventListener('click', clickButton);
+// 		$paper.addEventListener('click', clickButton);
+// 	}, 1000);
+	
+// } 위에 이런 방법도 있지만 이벤트를 추가했다 삭제했다 반복하면 오류가 생길 수 도 있기때문에
+let clickable = true;
+const clickButton = () =>{
+	if(clickable){
 		clearInterval(intervalId);
-		intervalId = setInterval(changeComputerHand, 50);
-	}, 1000);
+		clickable = false;
+		// 점수 계산 및 화면 표시
+		setTimeout(()=>{
+			clickable = true;
+			intervalId = setInterval(changeComputerHand, 50);
+		}, 1000);
+	}
 }
+// 이 방법을 선호 플래그 변수임
 
 $rock.addEventListener('click', clickButton);
 $scissors.addEventListener('click', clickButton);
