eaf21e8bbefede1553d515a0c85c61fbfe6e1617
woojin <76862976+stalexjr@users.noreply.github.com>
2022-12-23 06:19:22 +0900

    Add files via upload

--- 틱택토게임/js/script.js
+++ 틱택토게임/js/script.js
@@ -5,7 +5,9 @@ for (let i = 0; i < 3; i++) {
 	data.push([]);
 }
 
+const { body } = document;
 const $table = document.createElement('table');
+const $result = document.createElement('div');
 
 for (let i = 0; i < 3; i++) {
 	const $tr = document.createElement('tr');
@@ -28,5 +30,12 @@ for (let i = 0; i < 3; i++) {
 	$table.append($tr);
 }
 document.body.append($table);
+document.body.append($result);
+
+// 구조 분해 할당 문법이라는 것이 있는데
+// const { a, b } = obj; 이것을 어떻게 해석해야하냐면
+// const a = obj.a		const b = obj.b로 해석한다 이 두줄을 간단하게 한 줄로 표현한것이다.
+// 만약 여기서 나는 const c = obj.c가 필요하다 싶으면 추가로 한줄을 치는것이아니라
+// const { a, b } = obj에서 c를 추가하면 된다. 그럼 const { a, b, c,} = obj로 표현할 수 있다.
 
 
