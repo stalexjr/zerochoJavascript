4b5ce7541bc3a59842ca94c754574519ba9236bd
woojin <76862976+stalexjr@users.noreply.github.com>
2023-01-13 13:52:29 +0900

    Add files via upload

--- 클래스텍스트RPG게임/js/script.js
+++ 클래스텍스트RPG게임/js/script.js
@@ -2,6 +2,30 @@ const $startSceern = document.querySelector('#start-screen');
 const $gameMenu = document.querySelector('#game-menu');
 const $battleMenu = document.querySelector('#battle-menu');
 const $heroName = document.querySelector('#hero-name');
+const $heroLevel = document.querySelector('#hero-level');
+const $heroHp = document.querySelector('#hero-hp');
+const $heroXp = document.querySelector('#hero-xp');
+const $heroAtt = document.querySelector('#hero-att');
+const $monsterName = document.querySelector('#monster-name');
+const $monsterHp = document.querySelector('#monster-hp');
+const $monsterAtt = document.querySelector('#monster-att');
+const $message = document.querySelector('#message');
+
+const hero = {
+	name : '',
+	lev : 1,
+	maxHp : 100,
+	hp : 100,
+	xp : 0,
+	att : 10,
+};
+
+let monster = null;
+const monsterList = [
+	{ name : '슬라임', hp : 25, att : 10, xp: 10},
+	{ name : '스켈레톤', hp : 50, att : 15, xp: 20},
+	{ name : '마왕', hp : 150, att : 35, xp: 50},
+];
 
 $startSceern.addEventListener('submit', (e) => {
 	e.preventDefault();
@@ -10,4 +34,5 @@ $startSceern.addEventListener('submit', (e) => {
 	$startSceern.style.display = 'none';
 	$gameMenu.style.display = 'block';
 	$heroName.textContent = name;
+	$heroLevel.textContent = `${hero.lev}Lev`;
 })
