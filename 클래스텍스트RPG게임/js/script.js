3ec71e96dcf110098df20eecaeb4c22a89f1ec67
woojin <76862976+stalexjr@users.noreply.github.com>
2023-01-15 19:45:22 +0900

    Add files via upload

--- 클래스텍스트RPG게임/js/script.js
+++ 클래스텍스트RPG게임/js/script.js
@@ -12,19 +12,19 @@ const $monsterAtt = document.querySelector('#monster-att');
 const $message = document.querySelector('#message');
 
 const hero = {
-	name : '',
-	lev : 1,
-	maxHp : 100,
-	hp : 100,
-	xp : 0,
-	att : 10,
+	name: '',
+	lev: 1,
+	maxHp: 100,
+	hp: 100,
+	xp: 0,
+	att: 10,
 };
 
 let monster = null;
 const monsterList = [
-	{ name : '슬라임', hp : 25, att : 10, xp: 10},
-	{ name : '스켈레톤', hp : 50, att : 15, xp: 20},
-	{ name : '마왕', hp : 150, att : 35, xp: 50},
+	{ name: '슬라임', hp: 25, att: 10, xp: 10 },
+	{ name: '스켈레톤', hp: 50, att: 15, xp: 20 },
+	{ name: '마왕', hp: 150, att: 35, xp: 50 },
 ];
 
 $startSceern.addEventListener('submit', (e) => {
@@ -35,9 +35,40 @@ $startSceern.addEventListener('submit', (e) => {
 	$gameMenu.style.display = 'block';
 	$heroName.textContent = name;
 	$heroLevel.textContent = `${hero.lev} Lev`;
-	$heroHp.textContent = `HP : ${hero.hp} / ${hero.maxHp}`;
-	$heroXp.textContent = `XP : ${hero.xp} / ${15 * hero.lev}`;
+	$heroHp.textContent = `HP : ${hero.hp}/${hero.maxHp}`;
+	$heroXp.textContent = `XP : ${hero.xp}/${15 * hero.lev}`;
 	$heroAtt.textContent = `ATT : ${hero.att}`;
 	hero.name = name;
+});
+
+$gameMenu.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const input = e.target['menu-input'].value;
+	if (input === '1') {
+		$gameMenu.style.display = 'none';
+		$battleMenu.style.display = 'block';
+		monster = JSON.parse(
+			JSON.stringify(monsterList[Math.floor(Math.random() * monsterList.length)])
+		);
+		monster.maxHp = monster.hp;
+		$monsterName.textContent = monster.name;
+		$monsterHp.textContent = `HP: ${monster.hp}/${monster.maxHp}`;
+		$monsterAtt.textContent = `ATT: ${monster.att}`;
+	} else if (input === '2') {
+
+	} else if (input === '3') {
+
+	}
+	$battleMenu.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const input = e.target['battle-input'].value;
+		if(input === '1'){
+
+		}else if(input === '2'){
+
+		}else if(input === '3'){
+			
+		}
+	});
 });
 
