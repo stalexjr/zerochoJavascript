14e4d5b1e920a6df896d10fa109e696d38ad33cd
woojin <76862976+stalexjr@users.noreply.github.com>
2023-01-17 12:47:28 +0900

    Add files via upload

--- 클래스텍스트RPG게임/js/script.js
+++ 클래스텍스트RPG게임/js/script.js
@@ -44,7 +44,7 @@ $startSceern.addEventListener('submit', (e) => {
 $gameMenu.addEventListener('submit', (e) => {
 	e.preventDefault();
 	const input = e.target['menu-input'].value;
-	if (input === '1') {
+	if (input === '1') {	//모험
 		$gameMenu.style.display = 'none';
 		$battleMenu.style.display = 'block';
 		monster = JSON.parse(
@@ -54,20 +54,32 @@ $gameMenu.addEventListener('submit', (e) => {
 		$monsterName.textContent = monster.name;
 		$monsterHp.textContent = `HP: ${monster.hp}/${monster.maxHp}`;
 		$monsterAtt.textContent = `ATT: ${monster.att}`;
-	} else if (input === '2') {
+		const monster1 = JSON.parse(JSON.stringify(monsterList[0]));
+		// 👆 깊은 복사의 특성은 앞을 바꿔도 뒤를 바꿔도 바뀌지 않는다. 우리는 객체안의 정보가 필요할 뿐 객체 전부가 필요하진않다. 객체를 변수에다 대입할 때는 항상 얘가 복사되어야 되는지 참조해야 되는지 잘 살펴봐야한다.
+		const monster2 = monsterList[0];
+		// 👆 참조 관계의 특성상 앞을 바꾸면 뒤도 바뀌고 뒤를 바꾸면 앞도 바뀌게 된다.
+		const monster3 = { ...monster[0] };
+		// 👆 새로운 객체 리터럴을 만들어주고 그 안에서 복사를 원하는 곳에 ...을 붙이면 된다. 이건 얕은 복사 라고한다. 얕은 복사와 깊은 복사의 차이점은 얕은 복사는 곁 껍데기만 복사한다. 그리고 내부는 참조 형태이다. 깊은 복사는 참조관계가 끊기고 전부다 복사되는 것을 의미한다.
+		monster1.name = '새 몬스터';
+		console.log(monsterList[0].name);		//슬라임
+		monster2.name = '새 몬스터';
+		console.log(monsterList[0].name);		//새 몬스터
+		console.log(monsterList[0] === monster1);		//false, 깊은 복사
+		console.log(monsterList[0] === monster2);		//true, 참조 관계
+	} else if (input === '2') {		//휴식
 
-	} else if (input === '3') {
+	} else if (input === '3') {		//종료
 
 	}
 	$battleMenu.addEventListener('submit', (e) => {
 		e.preventDefault();
 		const input = e.target['battle-input'].value;
-		if(input === '1'){
+		if (input === '1') {
 
-		}else if(input === '2'){
+		} else if (input === '2') {
 
-		}else if(input === '3'){
-			
+		} else if (input === '3') {
+
 		}
 	});
 });
