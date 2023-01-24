ac8fe621b7b3d69cf4a8b3bcc623c067ecfc0810
woojin <76862976+stalexjr@users.noreply.github.com>
2023-01-24 12:40:25 +0900

    Add files via upload

---
+++ 클래스텍스트RPG게임/js/script -class.js
@@ -0,0 +1,85 @@
+const $startSceern = document.querySelector('#start-screen');
+const $gameMenu = document.querySelector('#game-menu');
+const $battleMenu = document.querySelector('#battle-menu');
+const $heroName = document.querySelector('#hero-name');
+const $heroLevel = document.querySelector('#hero-level');
+const $heroHp = document.querySelector('#hero-hp');
+const $heroXp = document.querySelector('#hero-xp');
+const $heroAtt = document.querySelector('#hero-att');
+const $monsterName = document.querySelector('#monster-name');
+const $monsterHp = document.querySelector('#monster-hp');
+const $monsterAtt = document.querySelector('#monster-att');
+const $message = document.querySelector('#message');
+
+class Game {
+	constructor(name) {
+		this.monster = null;
+		this.hero = null;
+		this.monsterList = [
+			{ name: '슬라임', hp: 25, att: 10, xp: 10 },
+			{ name: '스켈레톤', hp: 50, att: 15, xp: 20 },
+			{ name: '마왕', hp: 150, att: 35, xp: 50 },
+		];
+	}
+}
+
+class Hero {
+	constructor(game, name) {
+		this.game = game;
+		this.name = name;
+		this.lev = 1;
+		this.maxHp = 100;
+		this.hp = 100;
+		this.xp = 0;
+		this.att = 10;
+	}
+	attack(target) {
+		target.hp -= this.att;
+	}
+	heal(monster) {
+		this.hp += 20;
+		this.hp -= monster.att;
+	}
+}
+class Monster{
+	constructor(game, name, hp, att, xp){
+		this.game = game;
+		this.name = name;
+		this.maxHp = hp;
+		this.xp = xp;
+		this.att = att;
+	}
+	attack(target){
+		target.hp -= this.att;
+	}
+}
+let game = null;
+
+$startSceern.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const name = e.target['name-input'].value;
+	game = new Game(name);
+});
+
+$gameMenu.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const input = e.target['menu-input'].value;
+	if (input === '1') {	//모험
+
+	} else if (input === '2') {		//휴식
+
+	} else if (input === '3') {		//종료
+
+	}
+	$battleMenu.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const input = e.target['battle-input'].value;
+		if (input === '1') {	//공격
+		} else if (input === '2') {	//회복
+
+		} else if (input === '3') {	//도망
+
+		}
+	});
+});
+
